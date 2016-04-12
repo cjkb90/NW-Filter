b6fdfe77185164f511b89203f26cedff11c1bfd5
Christopher Kennedy <cjkb90@users.noreply.github.com>
2016-04-11 23:38:56 -0400

    Server filter functional

---
+++ server/routes/apiIndex.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var db = require(path.join(__dirname, '..', 'index.js'));
+var Product = db.Product;
+
+router.get('/products/all', function(req, res){
+	Product.find()
+	.then(function(response){
+		res.send(response);
+	})
+});
+
+router.get('/products/letter/:letter', function(req,res){
+	//
+	var letter = new RegExp('^'+req.params.letter,'i');
+	Product.find({
+		"name": { "$regex": letter } 
+	})
+	.then(function(response){
+		res.send(response);
+	})
+});
+
+router.get('/products/add/:prodName', function(req, res){
+	var newProd = new Product({name: req.params.prodName})
+	newProd.save()
+	.then(function(response){
+		res.send(response);
+	})
+});
+
+module.exports = router;
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var path = require('path');
 
 var landingPage = path.join(__dirname, '..', '..', 'public/index.html');
-var Product = require(path.join(__dirname, '..', 'index.js'));
 
 router.use('/browser', express.static(path.join(__dirname, '..', '..', 'browser')));
+router.use('/api', require('./apiIndex.js'));
 
 router.get('/', function(req, res){
 	res.sendFile(landingPage);
